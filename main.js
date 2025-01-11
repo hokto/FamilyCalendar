c1f6c71e76fec19c16a81e61227d79a3015c5362
hokto <hokuto.k1441@gmail.com>
2025-01-12 02:16:45 +0900

    削除時のメッセージ表示の実装とリファクタリング

--- main.js
+++ main.js
@@ -2,18 +2,27 @@
 const CHANNEL_ACCESS_TOKEN=PropertiesService.getScriptProperties().getProperty("LINE_CHANNEL_ACCESS_TOKEN");
 
 // LINE user ID
-const user_id = PropertiesService.getScriptProperties().getProperty("LINE_BOT_USER_ID");
+const LINE_BOT_USER_ID = PropertiesService.getScriptProperties().getProperty("LINE_BOT_USER_ID");
+
+// LINE Messaging API: Reply
+const LINE_MESSAGING_REPLY_URL =  PropertiesService.getScriptProperties().getProperty("LINE_MESSAGING_REPLY_URL");
+
+// LINE Messaging API: Post
+const LINE_MESSAGING_POST_URL = PropertiesService.getScriptProperties().getProperty("LINE_MESSAGING_POST_URL");
 
 // 特定のグループでのみbotが使用できるように制限
-const GROUPID= PropertiesService.getScriptProperties().getProperty("MY_GROUP_ID_TEST");
+const GROUP_ID= PropertiesService.getScriptProperties().getProperty("MY_GROUP_ID_TEST");
 
 // 使用するgmailアカウント
-const GMAIL= PropertiesService.getScriptProperties().getProperty("MY_GMAIL_ACCOUNT");
+const MY_GMAIL_ACCOUNT= PropertiesService.getScriptProperties().getProperty("MY_GMAIL_ACCOUNT");
 
-const calender=CalendarApp.getCalendarById(GMAIL);
+const calender=CalendarApp.getCalendarById(MY_GMAIL_ACCOUNT);
 
 // 使用するGeminiのAPIキー
 const GEMINI_API = PropertiesService.getScriptProperties().getProperty("GEMINI_API_KEY");
+
+// GEMINI-1.5-flashにプロンプトを投げるurl
+const GEMINI_FLASH_URL = PropertiesService.getScriptProperties().getProperty("GEMINI_FLASH_URL");
 
 var today = new Date();
 
@@ -40,10 +49,6 @@ function getMyCalender(day=new Date(),fowardDay=7){
   return events_arr;
 }
 
-function myDebug(){
-  Logger.log(notifyEventsInWeek());
-}
-
 // 全角文字を半角文字に変換
 function em2half(str){
   return str.replace(/[０-９]/g,function(s){
@@ -52,14 +57,8 @@ function em2half(str){
 }
 
 // カレンダー登録処理
-function resisterCalender(json){
+function resisterCalender(json,replytoken){
   /*  登録フォーマット
-  *   登録
-  *   MM月DD日HH時MM分
-  *   Title
-  *   (Description)
-  *.  20241225JSON形式に改変
-  *   
   {
     "name": "register",
     "date": {
@@ -78,49 +77,31 @@ function resisterCalender(json){
   var startday = new Date(`${json['date']['start-date'].day} ${json['date']['start-date'].time}`);
   var endday = new Date(`${json['date']['end-date'].day} ${json['date']['end-date'].time}`);
   var title = json.title;
-  var message;
   try{
     var event = calender.createEvent(title,startday,endday);
-    //message += "登録しました。";
-    //message +="\n\n";
-    //message+=formatEvent(event);
-    message = generateFlexMessageToConfirmEvent(event);
+    var message = generateFlexMessageToConfirmEvent(event);
+    return JSON.stringify({
+        'replyToken': replytoken,
+        'messages': [{
+            'type': "flex",
+            'altText': "CalendarFlexMessage",
+            'contents': message,
+        },
+        ],
+      });
   }catch(err){
-    message += "問題が生じました。";
-    message += err.toString();
-  }
-  return message;
-}
-
-// カレンダー削除処理
-function removeCalender(cmd){
-  /*  削除フォーマット
-  *   削除
-  *   MM月DD日
-  *   Id
-  */
-  /*
-  * TODO
-  * ・削除処理ではIDを使って削除するイベントを指定するが、そのIDをどのように表示するか？そもそもlineユーザ側にIDを表示させるのか
-  * ・ユーザ追加後の最初のメッセージやrootからの修正などでのメッセージ
-  */
-  var day = em2half(cmd[1]);
-  var splited_day = day.split(/[^(0-9)]/);
-  if(!splited_day[splited_day.length-1]){
-    splited_day.pop();
-  }
-  var format_day = splited_day.join("/");
-
-  var events = getMyCalender(new Date(format_day),1);
-  var events_size = events.length;
-  if(1<=parseInt(cmd[2])&&parseInt(cmd[2])<=events_size){
-    events[parseInt(cmd[2])-1].deleteEvent();
-    return "予定が削除されました。";
-  }
-  else{
-    return "その予定は存在しません。";
+    // TODO: シートにエラーキャッチできるようにした方がいい？
+    return JSON.stringify({
+      'replyToken': replytoken,
+      'messages': [{
+        'type' : 'text',
+        'text' : "エラーが発生しました．もう一度試してください．",
+      },
+    ]
+    });
   }
 }
+
 // カレンダー表示機能
 function printCalender(cmd){
   /*  表示フォーマット
@@ -156,47 +137,105 @@ function printCalender(cmd){
   return message;
 }
 
-// 要求された内容をスプレッドシートに追記していく
-function requiredFunction(cmd){
-  return "要望を受け付けました。\nご協力ありがとうございます。";
-}
-
-// デバッグ用の関数
-function debugMode(json,receivedmessage){
-  receivedmessage.shift();
-  var groupid=json.events[0].source.groupId;
-  var message="To Root Message:\n";
-  switch(receivedmessage[0]){
-    case "getgroupid":
-      message += "This group id is "+groupid;
-      break;
-    case "print":
-      message += printCalender(receivedmessage);
-      break;
-    case "resister":
-      message += resisterCalender(receivedmessage);
-      break;
-    case "remove":
-      message += removeCalender(receivedmessage);
-      break;
-    case "require":
-      message += requiredFunction(receivedmessage);
-      break;
-    case "todayevents":
-      notifyEventsInDay();
-      message += "Notify Today Events";
-      break;
-    case "weekevents":
-      notifyEventsInWeek();
-      message += "Notify Week Events";
-      break;
-    default:
-      message += "None";
-      break;
+function deleteEventById(event_id){
+  var event = calender.getEventById(event_id);
+  var handle_delete_message;
+  try{
+    event.deleteEvent();
+    handle_delete_message = "このイベントを削除しました．"
   }
-  return message;
+  catch(error){
+    // TODO: エラー専用のスプレッドシートに内容書き込むように
+    handle_delete_message = "イベントの削除に失敗しました．"
+  }
+  return handle_delete_message;
+}
+// イベント削除ボタンのイベントハンドリング
+function handleDeleteButtonClick(postback_data,replytoken){
+    // イベントidを取得してそのイベントを削除する．
+    var event_id = postback_data.data;
+    var message = deleteEventById(event_id);
+    return JSON.stringify({
+      'replyToken': replytoken,
+      'messages':[
+        {
+          'type' : 'text',
+          'text' : message,
+        },
+      ]
+    });
 }
 
+// メッセージから命令内容を検出する
+function detectCalendarCommandByMessage(received_message){
+    const prompt_to_gemini = `文を以下に従うように分析して出力してください．
+  1. メッセージ内容から，ユーザがカレンダーに内容を追加して欲しい意図を含む文であるかどうかを判定する．
+  もし追加して欲しい意図を含む内容ではないと判定すれば，
+  {
+    "name": "undefind"
+  }
+  を返して終了する．
+  もし追加して欲しい意図を含む内容であると判定すれば2.に進む．
+  2. メッセージ内容から「予定の開始日」,「予定の開始時間」，「予定の終了日」，「予定の終了時間」，「予定内容」を抽出する．
+  ここで現在の日付は「${today.getFullYear()}/${today.getMonth()+1}/${today.getDate()}」であり，
+  現在時刻は「${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}」であるとする．
+  ただし，「予定の開始日」「予定の終了日」は，現在の日付を基準にして最も近い未来の日付になるように調整すること．
+  つまり，「現在の日付」に対して「予定の開始日」が過去の日付になっていれば，「未来の開始日」の西暦を「現在の日付」に対して来年にする．
+  もし「予定の開始時間」がなければ"00:00:00"として扱う．
+  もし「予定の終了日」がなければ「予定の開始日」と同じ日として扱う．
+  もし「予定の終了時間」がなければ「予定の開始時間」の一時間後として扱う．
+
+  3.に進む
+  3. 
+  {
+      "name": "register",
+      "date": {
+        "start-date":{
+          "day": "予定の開始日",
+          "time": "予定の開始時間",
+        },
+        "end-date": {
+          "day": "予定の終了日",
+          "time": "予定の終了時間",
+        },
+      },
+      "title": "予定内容",
+  }
+  上の形式に沿って出力すること．この際，一切の改行を行わなくて良い．
+  それ以外の出力は行わず，余計な情報の付与も必要ない．
+  また，「予定の開始日」「予定の終了日」は"YYYY/MM/DD"に合うようにすること．
+  「予定の開始時間」「予定の終了時間」は"HH:MM:SS"に合うようにすること．`;
+
+    const url = `${GEMINI_FLASH_URL}?key=${GEMINI_API}`
+          , payload = {
+              'contents': [
+                {
+                  'parts': [{
+                    'text': received_message
+                  }],
+                  "role": "user"
+                }
+              ],
+              "systemInstruction":{
+                  'parts':[{
+                    'text': prompt_to_gemini
+                  }],
+                  "role": "model"
+              },
+            },
+    options = {
+              'method': 'post',
+              'contentType': 'application/json',
+              'payload': JSON.stringify(payload)
+            };
+
+    var detected_result = UrlFetchApp.fetch(url, options);
+    var detected_result_json = JSON.parse(detected_result.getContentText());
+    var response_json_text = detected_result_json.candidates[0].content.parts[0].text;
+    response_json_text = response_json_text.toString().replace(/json/g,'').replace(/\`\`\`/g,''); // コードブロックの出力を置き換え
+    
+    return JSON.parse(response_json_text)
+}
 
 function doPost(e) {
   var json=JSON.parse(e.postData.contents);
@@ -204,136 +243,34 @@ function doPost(e) {
   if (typeof replytoken === 'undefined') {
     return;
   }
-  var line_messaging_url = 'https://api.line.me/v2/bot/message/reply';
-  var groupid=json.events[0].source.groupId;
-  if(groupid != GROUPID){
-    // 一旦余計なapi呼び出し生まないために，テストグループ以外は実行しないようにする
+  var group_id=json.events[0].source.groupId;
+  if(group_id != GROUP_ID){
+    // 一旦余計なapi呼び出し生まないために，決めたグループ以外は実行しないようにする
     return
   }
   var event_type = json.events[0].type;
+  var line_messaging_payload;
   if(event_type == "postback"){
-    // イベントidを取得してそのイベントを削除する．
-    var event_id = json.events[0].postback.data;
-    var event = calender.getEventById(event_id);
-    event.deleteEvent();
-    return;
-  }
-  var receivedmessage=json.events[0].message.text;
-  const prompt = `文を以下に従うように分析して出力してください．
-1. メッセージ内容から，ユーザがカレンダーに内容を追加して欲しい意図を含む文であるかどうかを判定する．
-例えば，「XX月XX日にXXにいく予定を追加」といったものはユーザがカレンダーに日程を追加して欲しい意図を含んでいると判断できる．
-しかし，「XX月XX日にXXにいく予定あったよね？」といったものはユーザがカレンダーに日程を追加して欲しいのではなく，
-他のユーザに日程の有無を聞いているだけであるためそういった意図は含んでいないと判断できる．
-もし追加して欲しい意図を含む内容ではないと判定すれば，
-{
-  "name": "undefind"
-}
-を返して終了する．
-もし追加して欲しい意図を含む内容であると判定すれば2.に進む．
-2. メッセージ内容から「予定の開始日」,「予定の開始時間」，「予定の終了日」，「予定の終了時間」，「予定内容」を抽出する．
-ここで現在の日付は「${today.getFullYear()}/${today.getMonth()+1}/${today.getDate()}」であり，
-現在時刻は「${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}」であるとする．
-"明日"や"今日"という現在の日付に近しい日付を指す単語が含まれている場合は，現在の日付を基準にして特定の日付に変更して解釈すること．
-ただし，「予定の開始日」「予定の終了日」は，現在の日付を基準にして最も未来の日付になるように調整すること．
-例えば，現在の日付が「2024/12/31」であり予定の開始日が「1/1」である場合には，来年の予定であると判定できるため，
-予定の開始日を「2025/01/01」と修正する．
-ただし，現在の日付が「2024/12/30」であり予定の開始日が「12/31」である場合には，今年の予定の可能性が高いと判定できるため，
-予定の開始日を「2024/12/31」と設定し修正してはいけない．
-もし「予定の開始時間」がなければ"00:00:00"として扱う．
-もし「予定の終了日」がなければ「予定の開始日」と同じ日として扱う．
-もし「予定の終了時間」がなければ"23:59:59"として扱う．
-
-3.に進む
-3. 
-{
-    "name": "register",
-    "date": {
-      "start-date":{
-        "day": "予定の開始日",
-        "time": "予定の開始時間",
-      },
-      "end-date": {
-        "day": "予定の終了日",
-        "time": "予定の終了時間",
-      },
-    },
-    "title": "予定内容",
-}
-上の形式に沿って出力すること．この際，一切の改行を行わなくて良い．
-それ以外の出力は行わず，余計な情報の付与も必要ない．
-また，「予定の開始日」「予定の終了日」は"YYYY/MM/DD"に合うようにすること．
-「予定の開始時間」「予定の終了時間」は"HH:MM:SS"に合うようにすること．`;
-
-  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API}`
-        , payload = {
-            'contents': [
-              {
-                'parts': [{
-                  'text': receivedmessage
-                }],
-                "role": "user"
-              }
-            ],
-            "systemInstruction":{
-                'parts':[{
-                  'text': prompt
-                }],
-                "role": "model"
-            },
-          },
-  options = {
-            'method': 'post',
-            'contentType': 'application/json',
-            'payload': JSON.stringify(payload)
-          };
-
-  var res = UrlFetchApp.fetch(url, options);
-  var res_json = JSON.parse(res.getContentText());
-  var response_json_text = res_json.candidates[0].content.parts[0].text;
-  response_json_text = response_json_text.toString().replace(/json/g,'').replace(/\`\`\`/g,''); // コードブロックの出力を置き換え
-  
-  var message;
-  var response_json_parsed = JSON.parse(response_json_text)
-  //message += response_json_text;
-  
-  //console.log(JSON.stringify(resJson));
-  //message += JSON.stringify(response_json_parsed);
-  if(response_json_parsed.name == "debug"){
-    message = debugMode(json,receivedmessage);
-  }
-  else if(groupid!=GROUPID){
-    message = "このグループで使用することはできません。";
+    line_messaging_payload = handleDeleteButtonClick(json.events[0].postback,replytoken);
   }
   else{
-    switch(response_json_parsed.name){
+    var received_message=json.events[0].message.text;
+    var parsed_response_json = detectCalendarCommandByMessage(received_message)
+    switch(parsed_response_json.name){
       case "register":
-          message = resisterCalender(response_json_parsed);
+          line_messaging_payload = resisterCalender(parsed_response_json,replytoken);
           break;
       case "削除":
-          message = removeCalender(receivedmessage);
+          message = removeCalender(received_message);
           break;
       case "表示":
-          message = printCalender(receivedmessage);
-          break;
-      case "要望":
-          message = requiredFunction(receivedmessage);
+          message = printCalender(received_message);
           break;
       default:
-          message = "適切なコマンドを入力してください。";
           break;
-          //return;
     }
   }
-  var line_messaging_payload = JSON.stringify({
-      'replyToken': replytoken,
-      'messages': [{
-          'type': "flex",
-          'altText': "FlexMessageが表示できません．",
-          'contents': message,
-      },
-      ],
-    });
-  UrlFetchApp.fetch(line_messaging_url, {
+  UrlFetchApp.fetch(LINE_MESSAGING_REPLY_URL, {
     'headers': {
       'Content-Type': 'application/json; charset=UTF-8',
       'Authorization': 'Bearer ' + CHANNEL_ACCESS_TOKEN,
@@ -361,11 +298,8 @@ function notifyEventsInDay(){
     return 0;
   }
 
-
-  var url = 'https://api.line.me/v2/bot/message/push';
-
   var payload = {
-    to: GROUPID,
+    to: GROUP_ID,
     messages: [
       { type: 'text', text: message }
     ]
@@ -380,45 +314,7 @@ function notifyEventsInDay(){
     payload: JSON.stringify(payload)
   };
 
-  UrlFetchApp.fetch(url, params);
-}
-
-// トリガーにより毎週日曜日に実行する関数
-function notifyEventsInWeek(){
-  var message = "おはようございます。\n今週の予定は、以下のようになっています。\n"
-  message += "---------------------------\n";
-  var events_week = getMyCalender(tommorow,7-1);
-  var events_week_arr =[];
-  events_week.forEach(event => events_week_arr.push(formatEvent(event)));
-  message += events_week_arr.join("\n");
-  message += "\n";
-  message += "---------------------------";
-
-
-  // 予定がない場合、何も表示しない
-  if(events_week_arr.length==0){
-    return 0;
-  }
-
-  var url = 'https://api.line.me/v2/bot/message/push';
-
-  var payload = {
-    to: GROUPID,
-    messages: [
-      { type: 'text', text: message }
-    ]
-  };
-
-  var params = {
-    method: 'post',
-    contentType: 'application/json',
-    headers: {
-      Authorization: 'Bearer ' + CHANNEL_ACCESS_TOKEN
-    },
-    payload: JSON.stringify(payload)
-  };
-
-  UrlFetchApp.fetch(url, params);
+  UrlFetchApp.fetch(LINE_MESSAGING_POST_URL, params);
 }
 
 function debugBySpreadSheet(url,debug_str){
